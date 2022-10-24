7372de57a480e2f29623395d6c0fed6ebc3cd804
qkstngus000 <qkstngus000@gmail.com>
2022-10-24 14:05:36 -0700

    working progress

--- Ass2/Client/client.c
+++ Ass2/Client/client.c
@@ -10,29 +10,42 @@
 #define SERVER_PORT 20984
 #define RECEIVE_BUFFER_SIZE 1024
 
-int main(int argc, char const *argv[]) {
-    //1. Server Socket address initiation
-    //// Clients would need server end point
-    struct sockaddr_in server_endpt;
-    memset(&server_endpt, 0, sizeof(struct sockaddr_in));
+
+int connectServer() {
+    //1. Server/Client Socket address initiation
+    //// Server/Client would need each other's end point
+    struct sockaddr_in server_socket_endpt;
+    memset(&server_socket_endpt, 0, sizeof(struct sockaddr_in));
+    struct sockaddr_in client_socket_endpt;
+    memset(&client_socket_endpt, 0, sizeof(struct sockaddr_in));
+
+    socklen_t client_endpt_length = sizeof(client_socket_endpt);
 
     // Create Socket with server socket addr
     ////AF_INET = Use IPv4     AF_INET6 = Use IPv6
     ////SOCK_STREAM = Use TCP   SOCK_DGRAM = Use UDP
     ////Protocol = 0 (Protocol Val for IP = 0)
-    int socket_desc = socket(AF_INET, SOCK_STREAM, 0);
+    int client_socket_desc = socket(AF_INET, SOCK_STREAM, 0);
 
-    server_endpt.sin_family = AF_INET;
-    server_endpt.sin_addr.s_addr = inet_addr(SERVER_IP_ADDRESS);
-    server_endpt.sin_port = htons(SERVER_PORT);
+    server_socket_endpt.sin_family = AF_INET;
+    server_socket_endpt.sin_addr.s_addr = inet_addr(SERVER_IP_ADDRESS);
+    server_socket_endpt.sin_port = htons(SERVER_PORT);
     
-    char *server_ip = inet_ntoa(server_endpt.sin_addr);
+    char *server_ip = inet_ntoa(server_socket_endpt.sin_addr);
 
-    printf("Connecting to server: %s:%d\n", server_ip, ntohs(server_endpt.sin_port));
+    printf("Connecting to server: %s:%d\n", server_ip, ntohs(server_socket_endpt.sin_port));
+    if(connect(client_socket_desc, (struct sockaddr *)&server_socket_endpt, sizeof(struct sockaddr_in)) < 0) {
+        perror("Connection Failure");
+    }
+    if (getsockname(client_socket_desc, (struct sockaddr *)&client_socket_endpt, &client_endpt_length) < 0) {
+        perror("getsockname Failure");
+    }
     
 
+    return 0;
+}
 
-
-    
-
+int main(int argc, char const *argv[]) {
+    connectServer();
+    return 0;
 }
--- Ass2/Server/server.c
+++ Ass2/Server/server.c
@@ -10,8 +10,32 @@
 #define RECEIVE_BUFFER_SIZE 1024
 
 int main(int argc, char const *argv[]) {
+    connectClient();
+    return 0;
+}
 
+int connectClient() {
     // Server would need both of client and server end points
-    printf("Hello WOrld\n");
+    printf("Hello World\n");
+    struct sockaddr_in server_socket_endpt;
+    memset(&server_socket_endpt, 0, sizeof(struct sockaddr_in));
+    struct sockaddr_in client_socket_endpt;
+    memset(&client_socket_endpt, 0, sizeof(struct sockaddr_in));
 
+    int server_socket_desc = socket(AF_INET, SOCK_STREAM, 0);
+
+    server_socket_endpt.sin_family = AF_INET;
+    server_socket_endpt.sin_addr.s_addr = inet_addr(SERVER_IP_ADDRESS);
+    server_socket_endpt.sin_port = htons(SERVER_PORT);
+
+    // bind: Assign name of socket
+    // USAGE: bind(int sockfd, const struct sockaddr *addr, socklen_t addrlen);
+    if (bind(server_socket_desc, &server_socket_endpt, sizeof(struct sockaddr_in)) < 0) {
+        perror("Bind Failure");
+    }
+    
+    // listen: mark socket that will be used to accpet incoming connection request
+    if (listen(server_socket_desc, 10) < 0) {
+        perror("Listen Failure");
+    } 
 }
