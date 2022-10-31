b2f5087284ec6a9b3b3d0dfa85d97cb5f7e5d2c9
root <root@DESKTOP-3P53CTS.localdomain>
2022-10-31 10:33:03 -0700

    test files added

---
+++ Ass2/Client_myown/client.c
@@ -0,0 +1,76 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include "Utilities.h"
+
+#define TRUE 1
+#define SERVER_IP_ADDRESS "127.0.0.1"
+#define SERVER_PORT 20984
+#define RECEIVE_BUFFER_SIZE 1024
+#define SEND_BUFFER_SIZE 1024
+
+
+int connectServer() {
+    //1. Server/Client Socket address initiation
+    //// Server/Client would need each other's end point
+    int buffersize = 1024;
+
+    struct sockaddr_in server_socket_endpt;
+    memset(&server_socket_endpt, 0, sizeof(struct sockaddr_in));
+    struct sockaddr_in client_socket_endpt;
+    memset(&client_socket_endpt, 0, sizeof(struct sockaddr_in));
+
+    socklen_t client_endpt_length = sizeof(client_socket_endpt);
+
+    // Create Socket with server socket addr
+    ////AF_INET = Use IPv4     AF_INET6 = Use IPv6
+    ////SOCK_STREAM = Use TCP   SOCK_DGRAM = Use UDP
+    ////Protocol = 0 (Protocol Val for IP = 0)
+    int client_socket_desc = socket(AF_INET, SOCK_STREAM, 0);
+
+    server_socket_endpt.sin_family = AF_INET;
+    server_socket_endpt.sin_addr.s_addr = inet_addr(SERVER_IP_ADDRESS);
+    server_socket_endpt.sin_port = htons(SERVER_PORT);
+    
+    char *server_ip = inet_ntoa(server_socket_endpt.sin_addr);
+
+    printf("Connecting to server: %s:%d\n", server_ip, ntohs(server_socket_endpt.sin_port));
+    if(connect(client_socket_desc, (struct sockaddr *)&server_socket_endpt, sizeof(struct sockaddr_in)) < 0) {
+        perror("Connection Failure");
+    }
+    if (getsockname(client_socket_desc, (struct sockaddr *)&client_socket_endpt, &client_endpt_length) < 0) {
+        perror("getsockname Failure");
+    }
+    char *client_ip = inet_ntoa(client_socket_endpt.sin_addr);
+    printf("Client Endpoint: %s:%d\n", client_ip, ntohs(client_socket_endpt.sin_port));
+    
+    char receive_buffer[RECEIVE_BUFFER_SIZE] = {0};
+    char *send_buffer;
+    while (TRUE) {
+
+        // Receieve server dialogue
+        memset(receive_buffer, 0, RECEIVE_BUFFER_SIZE);
+        if (read(client_socket_desc, receive_buffer, RECEIVE_BUFFER_SIZE) < 0) perror("Read Failure");
+        else printf("%s\n", receive_buffer);
+        memset(receive_buffer, 0, RECEIVE_BUFFER_SIZE);
+        
+        // Respond to server
+        
+        memset(send_buffer, 0, SEND_BUFFER_SIZE);
+        if (getline(&send_buffer, &buffersize, stdin) < 0) perror("message write failed");
+        printf("%s", send_buffer);
+        if (write(client_socket_desc, &send_buffer, sizeof(char)) < 0) perror("write to server failure");
+
+        printf("write to server completed");
+    }
+        
+    return 0;
+}
+
+int main(int argc, char const *argv[]) {
+    connectServer();
+    return 0;
+}
---
+++ Ass2/Server_myown/server.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include "Utilities.h"
+
+#define TRUE 1
+#define SERVER_IP_ADDRESS "0.0.0.0"
+#define SERVER_PORT 20984
+#define RECEIVE_BUFFER_SIZE 1024
+#define SEND_BUFFER_SIZE 1024
+
+
+int connectClient() {
+    // Server would need both of client and server end points
+    printf("Hello World\n");
+    struct sockaddr_in server_socket_endpt;
+    memset(&server_socket_endpt, 0, sizeof(struct sockaddr_in));
+    struct sockaddr_in client_socket_endpt;
+    memset(&client_socket_endpt, 0, sizeof(struct sockaddr_in));
+
+    int server_socket_desc = socket(AF_INET, SOCK_STREAM, 0);
+
+    server_socket_endpt.sin_family = AF_INET;
+    server_socket_endpt.sin_addr.s_addr = inet_addr(SERVER_IP_ADDRESS);
+    server_socket_endpt.sin_port = htons(SERVER_PORT);
+
+    // bind: Assign name of socket
+    // USAGE: bind(int sockfd, const struct sockaddr *addr, socklen_t addrlen);
+    if (bind(server_socket_desc, (struct sockaddr *)&server_socket_endpt, sizeof(struct sockaddr_in)) < 0) {
+        perror("Bind Failure");
+    }
+    
+    // listen: mark socket that will be used to accpet incoming connection request
+    if (listen(server_socket_desc, 10) < 0) {
+        perror("Listen Failure");
+    }
+
+    char receive_buffer[RECEIVE_BUFFER_SIZE] = {0};
+    char send_buffer[SEND_BUFFER_SIZE] = {0};
+    // Wait for client response
+    while (TRUE) {
+        printf("Waiting for client connection...\n");
+
+        socklen_t sock_len = sizeof(struct sockaddr_in);
+        int client_socket_desc = accept(server_socket_desc, (struct sockaddr *)&client_socket_endpt, &sock_len);
+        
+        char *client_ip = inet_ntoa(client_socket_endpt.sin_addr);
+
+        printf("Accepted connection: %s:%d\n\n", client_ip, ntohs(client_socket_endpt.sin_port));
+
+        int quit = -1;
+
+        // Counts for how many times user has solved proglems
+        int counter = 0;
+
+        while (quit !=0) {
+            const char starting_prompt[] = "Would you like to play (y/n)?\0";
+            if (write(client_socket_desc, starting_prompt, strlen(starting_prompt)) < 0) perror("Write Failure");
+            
+
+            memset(receive_buffer, 0, RECEIVE_BUFFER_SIZE);
+            if (read(client_socket_desc, receive_buffer, sizeof(char))< 0) perror("Read Failure");
+            else printf("Receieved from client: %s", receive_buffer);
+        }
+        
+
+    }
+
+}
+
+int main(int argc, char const *argv[]) {
+    connectClient();
+    return 0;
+}
